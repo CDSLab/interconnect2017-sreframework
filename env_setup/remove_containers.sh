d1f15e4f861fffe3fc2d7bd867329bc831cccd4a
Krunal Vora <kmvora@us.ibm.com>
2017-03-19 14:24:06 -0700

    cleanup files

--- env_setup/remove_containers.sh
+++ env_setup/remove_containers.sh
@@ -7,25 +7,49 @@ remove_containers()
 echo ">> Remvoving the containers..." | tee -a $LOG_FILE
 
 if docker ps | grep "$APACHE_1" >>$LOG_FILE 2>&1
-then docker rm -f $APACHE_1 >>$LOG_FILE 2>&1
+then
+  if docker rm -f $APACHE_1 >>$LOG_FILE 2>&1
+  then echo ">> Container $APACHE_1 removed successfully." | tee -a $LOG_FILE
+  else
+    echo ">> Error removing the container $APACHE_1. One or more Containers might not exist. Check logs at '$LOG_FILE'" | tee -a $LOG_FILE;
+    exit 1;
+  fi
 else echo ">> Container $APACHE_1 not present." | tee -a $LOG_FILE
 fi
 
 
 if docker ps | grep "$APACHE_2" >>$LOG_FILE 2>&1
-then docker rm -f $APACHE_2 >>$LOG_FILE 2>&1
+then
+  if docker rm -f $APACHE_2 >>$LOG_FILE 2>&1
+  then echo ">> Container $APACHE_2 removed successfully." | tee -a $LOG_FILE
+  else
+    echo ">> Error removing the container $APACHE_2. One or more Containers might not exist. Check logs at '$LOG_FILE'" | tee -a $LOG_FILE;
+    exit 1;
+  fi
 else echo ">> Container $APACHE_2 not present." | tee -a $LOG_FILE
 fi
 
 
 if docker ps | grep "$NGINX" >>$LOG_FILE 2>&1
-then docker rm -f $NGINX >>$LOG_FILE 2>&1
+then
+  if docker rm -f $NGINX >>$LOG_FILE 2>&1
+  then echo ">> Container $NGINX removed successfully." | tee -a $LOG_FILE
+  else
+    echo ">> Error removing the container $NGINX. One or more Containers might not exist. Check logs at '$LOG_FILE'" | tee -a $LOG_FILE;
+    exit 1;
+  fi
 else echo ">> Container $NGINX not present." | tee -a $LOG_FILE
 fi
 
 
 if docker ps | grep "$CADVISOR" >>$LOG_FILE 2>&1
-then docker rm -f $CADVISOR >>$LOG_FILE 2>&1
+then
+  if docker rm -f $CADVISOR >>$LOG_FILE 2>&1
+  then echo ">> Container $CADVISOR removed successfully." | tee -a $LOG_FILE
+  else
+    echo ">> Error removing the container $CADVISOR. One or more Containers might not exist. Check logs at '$LOG_FILE'" | tee -a $LOG_FILE;
+    exit 1;
+  fi
 else echo ">> Container $CADVISOR not present." | tee -a $LOG_FILE
 fi
 

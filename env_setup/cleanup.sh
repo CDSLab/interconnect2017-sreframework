efd2ddbb426cc3414519e4de53b6211578f4be6c
Krunal Vora <kmvora@us.ibm.com>
2017-03-19 14:20:06 -0700

    cleanup files

--- env_setup/cleanup.sh
+++ env_setup/cleanup.sh
@@ -7,7 +7,7 @@ echo "\n\n\nTime: $(date)\n\n\n" >>$LOG_FILE
 echo "\n**************** INITIATING CLEANUP ***********************"
 
 remove_containers
-remove_images
+# remove_images
 
 # rm $LOG_FILE
 

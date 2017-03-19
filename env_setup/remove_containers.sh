829a0ec46c89124354c47af8d1cc4b699b950c8f
Krunal Vora <kmvora@us.ibm.com>
2017-03-19 14:16:46 -0700

    cleanup files

--- env_setup/remove_containers.sh
+++ env_setup/remove_containers.sh
@@ -5,14 +5,39 @@
 remove_containers()
 {
 echo ">> Remvoving the containers..." | tee -a $LOG_FILE
-if docker rm -f $APACHE_1 >>$LOG_FILE 2>&1 &&
-   docker rm -f $APACHE_2 >>$LOG_FILE 2>&1 &&
-   docker rm -f $NGINX >>$LOG_FILE 2>&1 &&
-   docker rm -f $CADVISOR >>$LOG_FILE 2>&1
-then
-  echo ">> Containers removed successfully." | tee -a $LOG_FILE
-else
-  echo ">> Error removing the containers. One or more Containers might not exist. Check logs at '$LOG_FILE'" | tee -a $LOG_FILE;
-  # exit 1;
+
+if docker ps | grep "$APACHE_1" >>$LOG_FILE 2>&1
+then docker rm -f $APACHE_1 >>$LOG_FILE 2>&1
+else echo ">> Container $APACHE_1 not present." | tee -a $LOG_FILE
 fi
+
+
+if docker ps | grep "$APACHE_2" >>$LOG_FILE 2>&1
+then docker rm -f $APACHE_2 >>$LOG_FILE 2>&1
+else echo ">> Container $APACHE_2 not present." | tee -a $LOG_FILE
+fi
+
+
+if docker ps | grep "$NGINX" >>$LOG_FILE 2>&1
+then docker rm -f $NGINX >>$LOG_FILE 2>&1
+else echo ">> Container $NGINX not present." | tee -a $LOG_FILE
+fi
+
+
+if docker ps | grep "$CADVISOR" >>$LOG_FILE 2>&1
+then docker rm -f $CADVISOR >>$LOG_FILE 2>&1
+else echo ">> Container $CADVISOR not present." | tee -a $LOG_FILE
+fi
+
+#
+# if docker rm -f $APACHE_1 >>$LOG_FILE 2>&1 &&
+#    docker rm -f $APACHE_2 >>$LOG_FILE 2>&1 &&
+#    docker rm -f $NGINX >>$LOG_FILE 2>&1 &&
+#    docker rm -f $CADVISOR >>$LOG_FILE 2>&1
+# then
+#   echo ">> Containers removed successfully." | tee -a $LOG_FILE
+# else
+#   echo ">> Error removing the containers. One or more Containers might not exist. Check logs at '$LOG_FILE'" | tee -a $LOG_FILE;
+#   # exit 1;
+# fi
 }
--- env_setup/remove_images.sh
+++ env_setup/remove_images.sh
@@ -5,13 +5,53 @@
 remove_images()
 {
 echo "\n\n\n>> Remvoving the images..." | tee -a $LOG_FILE
-if docker rmi $APACHE_IMAGE >>$LOG_FILE 2>&1 &&
-   docker rmi $NGINX_IMAGE >>$LOG_FILE 2>&1 &&
-   docker rmi $CADVISOR_IMAGE >>$LOG_FILE 2>&1
+
+if docker images | grep "$APACHE_IMAGE" >>$LOG_FILE 2>&1
 then
-  echo ">> Images removed successfully." | tee -a $LOG_FILE
-else
-  echo ">> Error removing the images. One or more Images might not exist. Check logs at '$LOG_FILE'" | tee -a $LOG_FILE;
-  # exit 1;
+  if docker rmi $APACHE_IMAGE >>$LOG_FILE 2>&1
+  then echo ">> $APACHE_IMAGE removed successfully." | tee -a $LOG_FILE
+  else
+    echo ">> Error removing $APACHE_IMAGE. Check logs at '$LOG_FILE'" | tee -a $LOG_FILE;
+    exit 1;
+  fi
+else echo ">> Image $APACHE_IMAGE not present." | tee -a $LOG_FILE
 fi
+
+
+if docker images | grep "$NGINX_IMAGE" >>$LOG_FILE 2>&1
+then
+  if docker rmi $NGINX_IMAGE >>$LOG_FILE 2>&1
+  then echo ">> $NGINX_IMAGE removed successfully." | tee -a $LOG_FILE
+  else
+    echo ">> Error removing $NGINX_IMAGE. Check logs at '$LOG_FILE'" | tee -a $LOG_FILE;
+    exit 1;
+  fi
+else echo ">> Image $NGINX_IMAGE not present." | tee -a $LOG_FILE
+fi
+
+
+if docker images | grep "$CADVISOR_IMAGE" >>$LOG_FILE 2>&1
+then
+  if docker rmi $CADVISOR_IMAGE >>$LOG_FILE 2>&1
+  then echo ">> $CADVISOR_IMAGE removed successfully." | tee -a $LOG_FILE
+  else
+    echo ">> Error removing $CADVISOR_IMAGE. Check logs at '$LOG_FILE'" | tee -a $LOG_FILE;
+    exit 1;
+  fi
+else echo ">> Image $CADVISOR_IMAGE not present." | tee -a $LOG_FILE
+fi
+
+
+
+
+
+# if docker rmi $APACHE_IMAGE >>$LOG_FILE 2>&1 &&
+#    docker rmi $NGINX_IMAGE >>$LOG_FILE 2>&1 &&
+#    docker rmi $CADVISOR_IMAGE >>$LOG_FILE 2>&1
+# then
+#   echo ">> Images removed successfully." | tee -a $LOG_FILE
+# else
+#   echo ">> Error removing the images. One or more Images might not exist. Check logs at '$LOG_FILE'" | tee -a $LOG_FILE;
+#   # exit 1;
+# fi
 }
